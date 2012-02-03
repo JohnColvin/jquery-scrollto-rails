232572852bc91d1e81e841decc83a85bef149329
John Colvin <colvin.68@osu.edu>
2012-02-02 21:46:14 -0500

    Generated gem

---
+++ lib/jquery-scrollto-rails.rb
@@ -0,0 +1,9 @@
+require "jquery-scrollto-rails/version"
+
+module Jquery
+  module Scrollto
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jquery-scrollto-rails/version.rb
@@ -0,0 +1,7 @@
+module Jquery
+  module Scrollto
+    module Rails
+      VERSION = "0.0.1"
+    end
+  end
+end
